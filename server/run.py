838142fc538e950dab0bfa271cb5f7de592601a0
Silwal <sxs8672@mavs.uta.edu>
2025-11-03 09:14:34 -0600

    Changes made to run.py for development

--- server/run.py
+++ server/run.py
@@ -3,8 +3,9 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # debug True for local development
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # debug True for  development
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 # Running the python script on a virtual enviormnet backend
 # python3 -m venv venv (not required if you already have a venv)
